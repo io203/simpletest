ae013b6a9fd44949c3120914bee334fdb2bef431
남궁성환 <saturn203@naver.com>
2019-06-04 18:37:36 +0900

    6.7

--- src/main/java/com/mycom/demo/controller/SimpleController.java
+++ src/main/java/com/mycom/demo/controller/SimpleController.java
@@ -36,8 +36,8 @@ public class SimpleController {
 	
 	@GetMapping("/version")
 	public String version(){
-		log.info("version 7.0");
-		return "version 7.0";
+		log.info("version 6.7");
+		return "version 6.7";
 		
 	}
 	

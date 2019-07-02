a074c0a6641dc40f42d7cca8eccb37218c51bc80
남궁성환 <saturn203@naver.com>
2019-07-02 14:21:58 +0900

    111

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM java:8
-
-ADD target/simpletest-0.0.1-SNAPSHOT.jar simpletest.jar
-
-
-
-
-ENTRYPOINT ["java", "-jar", "./simpletest.jar"]
--- src/main/java/com/mycom/demo/controller/SimpleController.java
+++ src/main/java/com/mycom/demo/controller/SimpleController.java
@@ -36,8 +36,8 @@ public class SimpleController {
 	
 	@GetMapping("/version")
 	public String version(){
-		log.info("version 7.5");
-		return "version 7.5";
+		log.info("version 7.6");
+		return "version 7.6";
 		
 	}
 	
